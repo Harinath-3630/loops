8989e045f031da98dd3730f076a04395f2d6ee6c
Harinath <kathiarun3630@gmail.com>
2021-12-15 22:37:08 +0530

    loops

---
+++ index.js
@@ -0,0 +1,28 @@
+var a=6;
+for(let i=1; i<=a; i++){
+console.log("KATHI")
+}
+
+
+
+var a=6;
+for(let i=1; i<=a; i++){
+console.log(i)
+}
+
+
+
+var sum=0;
+var n=50;
+for (let i=1; i<=n; i++){
+    sum +=i;
+}
+console.log(sum)
+
+let i=1;
+n=5;
+while(i<=n) {
+    console.log(i)
+    i+=1;
+}
+
